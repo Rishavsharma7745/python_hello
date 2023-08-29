a425f0d2d5040de76067b7a285502887def33a62
Rishavsharma7745 <82111795+Rishavsharma7745@users.noreply.github.com>
2023-08-29 10:35:32 +0530

    Update app.py
    changed the msg

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hey dosto'
+    return 'Version three using docker-compose now'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
