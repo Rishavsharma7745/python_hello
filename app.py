84cd9ed6f28e311d0cc869129500e3898d375f19
Rishavsharma7745 <82111795+Rishavsharma7745@users.noreply.github.com>
2023-09-06 07:50:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Version three using docker-compose now'
+    return 'Changing the version'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
