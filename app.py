222dd22ca9682a071f9d9adbbca890c1eff5016a
Rishavsharma7745 <82111795+Rishavsharma7745@users.noreply.github.com>
2023-09-06 08:02:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Changing the version'
+    return 'Hey folks'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
